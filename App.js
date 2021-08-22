4ba704b14b9a18c33c322821780bdd533cf0cdb8
MacOS <>
2021-08-22 13:07:49 +0200

    added new tab navigator

--- App.js
+++ App.js
@@ -16,6 +16,7 @@ import LoadingScreen from "./Screens/Loading/Loading";
 import CommentsScreen from "./Screens/Main/CommentsScreen";
 import * as key from "./Firebase";
 import { color } from "react-native-reanimated";
+import { View, Text, Animated, Dimensions } from "react-native";
 
 if (!firebase.apps.length) {
   firebase.initializeApp(key.firebaseConfig);
@@ -27,61 +28,192 @@ const Drawer = createDrawerNavigator();
 
 //Tab bar navigation function
 function AuthStack() {
+  const tabOffsetValue = React.useRef(new Animated.Value(0)).current;
+
+  function getWidth() {
+    let width = Dimensions.get("window").width;
+    width = width - 60;
+    return width / 5;
+  }
   return (
-    <Tab.Navigator>
-      <Tab.Screen
-        name="Home"
-        component={HomeScreen}
-        options={{
-          tabBarIcon: () => (
-            <Icon name="home-outline" size={25} color="black" />
-          ),
+    <NavigationContainer independent={true}>
+      <Tab.Navigator
+        initialRouteName="Search"
+        tabBarOptions={{
+          style: {
+            position: "absolute",
+            bottom: 25,
+            left: 25,
+            right: 25,
+            borderRadius: 20,
+            shadowColor: "#7653D9",
+            shadowOpacity: 0.3,
+            shadowOffset: {
+              width: 10,
+              height: 10,
+            },
+            elevation: 4,
+          },
+          showLabel: false,
         }}
-      />
-      <Tab.Screen
-        name="Search"
-        component={SearchScreen}
-        options={{
-          tabBarIcon: () => <Icon name="search" size={25} color="black" />,
-        }}
-      />
-      <Tab.Screen
-        name="Add"
-        component={AddScreen}
-        options={{
-          tabBarIcon: () =>
-            Platform.OS == "ios" ? (
-              <Icon
-                name="add-circle"
-                size={60}
-                color="#7653D9"
-                style={{ height: 60 }}
-              />
-            ) : (
-              <Icon name="add-circle" size={60} color="#7653D9" />
+      >
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <View
+                style={{
+                  alignItems: "center",
+                  justifyContent: "center",
+                  top: "40%",
+                }}
+              >
+                <Icon name="home-outline" size={25} color="black" />
+                <Text
+                  style={{ fontSize: 10, color: focused ? "#7653D9" : "black" }}
+                >
+                  HOME
+                </Text>
+              </View>
             ),
-          title: "",
+          }}
+          listeners={({ NavigationContainer, route }) => ({
+            tabPress: (e) => {
+              Animated.spring(tabOffsetValue, {
+                toValue: 0,
+                useNativeDriver: true,
+              }).start();
+            },
+          })}
+        />
+        <Tab.Screen
+          name="Search"
+          component={SearchScreen}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <View
+                style={{
+                  alignItems: "center",
+                  justifyContent: "center",
+                  top: "40%",
+                }}
+              >
+                <Icon name="search" size={25} color="black" />
+                <Text
+                  style={{ fontSize: 10, color: focused ? "#7653D9" : "black" }}
+                >
+                  SEARCH
+                </Text>
+              </View>
+            ),
+          }}
+          listeners={({ NavigationContainer, route }) => ({
+            tabPress: (e) => {
+              Animated.spring(tabOffsetValue, {
+                toValue: getWidth() * 1,
+                useNativeDriver: true,
+              }).start();
+            },
+          })}
+        />
+        <Tab.Screen
+          name="Add"
+          component={AddScreen}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <View
+                style={{
+                  alignItems: "center",
+                  justifyContent: "center",
+                  top: "30%",
+                }}
+              >
+                <Icon
+                  name="add-circle"
+                  size={60}
+                  color="#7653D9"
+                  style={{ height: 60 }}
+                />
+              </View>
+            ),
+
+            title: "",
+          }}
+        />
+        <Tab.Screen
+          name="Following"
+          component={FollowingScreen}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <View
+                style={{
+                  alignItems: "center",
+                  justifyContent: "center",
+                  top: "40%",
+                }}
+              >
+                <Icon name="heart-outline" size={25} color="black" />
+                <Text
+                  style={{ fontSize: 10, color: focused ? "#7653D9" : "black" }}
+                >
+                  FOLLOWING
+                </Text>
+              </View>
+            ),
+          }}
+          listeners={({ NavigationContainer, route }) => ({
+            tabPress: (e) => {
+              Animated.spring(tabOffsetValue, {
+                toValue: getWidth() * 3,
+                useNativeDriver: true,
+              }).start();
+            },
+          })}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <View
+                style={{
+                  alignItems: "center",
+                  justifyContent: "center",
+                  top: "40%",
+                }}
+              >
+                <Icon name="person-outline" size={25} color="black" />
+                <Text
+                  style={{ fontSize: 10, color: focused ? "#7653D9" : "black" }}
+                >
+                  PROFILE
+                </Text>
+              </View>
+            ),
+          }}
+          listeners={({ NavigationContainer, route }) => ({
+            tabPress: (e) => {
+              Animated.spring(tabOffsetValue, {
+                toValue: getWidth() * 4,
+                useNativeDriver: true,
+              }).start();
+            },
+          })}
+        />
+      </Tab.Navigator>
+      <Animated.View
+        style={{
+          backgroundColor: "#7653D9",
+          height: 2,
+          width: getWidth() - 20,
+          position: "absolute",
+          bottom: 30,
+          left: 40,
+          transform: [{ translateX: tabOffsetValue }],
         }}
       />
-      <Tab.Screen
-        name="Following"
-        component={FollowingScreen}
-        options={{
-          tabBarIcon: () => (
-            <Icon name="heart-outline" size={25} color="black" />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Profile"
-        component={ProfileScreen}
-        options={{
-          tabBarIcon: () => (
-            <Icon name="person-outline" size={25} color="black" />
-          ),
-        }}
-      />
-    </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -90,7 +222,7 @@ function AuthStack() {
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator headerMode="none" initialRouteName="Login">
+      <Stack.Navigator headerMode="none" initialRouteName="Home">
         <Stack.Screen name="Loading" component={LoadingScreen} />
         <Stack.Screen name="Comments" component={CommentsScreen} />
         <Stack.Screen
