d2418ed2cd38721255fd0d6df9a6fc85d451f39a
Printjt <SPOTIFYPRINTJT@GMAIL.COM>
2021-11-09 20:57:55 +0200

    no message

--- Screens/Main/SearchScreen.js
+++ Screens/Main/SearchScreen.js
@@ -1,14 +1,130 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useEffect, useState, useRef } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  FlatList,
+  LogBox,
+  Dimensions,
+  TouchableOpacity,
+} from "react-native";
+import firebase from "firebase/app";
+import "firebase/storage";
+import "firebase/firestore";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Icon from "react-native-vector-icons/FontAwesome";
 
-export default function SearchScreen() {
+if (!firebase.apps.length) {
+  firebase.initializeApp(key);
+}
+LogBox.ignoreLogs([`Setting a timer for a long period`]);
+LogBox.ignoreLogs([`VirtualizedLists`]);
+
+const db = firebase.firestore();
+const { height, width } = Dimensions.get("screen");
+
+export default function SearchScreen({ navigation }) {
+  const [users, setUsers] = useState({});
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    const subscriber = db
+      .collection("test_users")
+      .onSnapshot((querySnapshot) => {
+        const users = [];
+
+        querySnapshot.forEach((documentSnapshot) => {
+          users.push({
+            ...documentSnapshot.data(),
+            key: documentSnapshot.id,
+          });
+        });
+
+        setUsers(users);
+      });
+    // Unsubscribe from events when no longer in use
+    return () => subscriber();
+  }, []);
+
+  function Item({ items }) {
+    return (
+      <TouchableOpacity
+        style={styles.users}
+        onPress={() => navigation.navigate("Profile", { name: items.name })}
+      >
+        <Text>{items.name}</Text>
+      </TouchableOpacity>
+    );
+  }
+
+  const change = (t) => {
+    db.collection("test_users")
+      .where("name", ">=", t)
+      .onSnapshot((querySnapshot) => {
+        const users = [];
+
+        querySnapshot.forEach((documentSnapshot) => {
+          users.push({
+            ...documentSnapshot.data(),
+            key: documentSnapshot.id,
+          });
+        });
+
+        setUsers(users);
+      });
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Search Screen!!!</Text>
-    </View>
+    <SafeAreaView>
+      <View style={styles.searchInput}>
+        <TextInput
+          placeholder="Search"
+          onChangeText={(text) => setName(text)}
+          onSubmitEditing={() => change(name)}
+        />
+        <TouchableOpacity
+          style={{ alignSelf: "center", marginRight: 5 }}
+          onPress={() => change(name)}
+        >
+          <Icon name="search" size={20} />
+        </TouchableOpacity>
+      </View>
+      <FlatList
+        data={users}
+        renderItem={({ item }) => <Item items={item} />}
+        style={{
+          height: "100%",
+          flexGrow: 0,
+          marginTop: 5,
+          marginLeft: width / 8,
+        }}
+        showsVerticalScrollIndicator={false}
+        verticalScrollIndicator={false}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: { flex: 1, justifyContent: "center", alignItems: "center" },
+  searchInput: {
+    borderWidth: 0.5,
+    borderRadius: 10,
+    width: "50%",
+    height: 35,
+    textAlign: "center",
+    marginLeft: width / 4,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  users: {
+    borderWidth: 0.5,
+    borderRadius: 10,
+    width: "85%",
+    height: 35,
+    marginTop: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
